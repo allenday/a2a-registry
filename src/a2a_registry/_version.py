31415a1db5719905d4d70e3ba82bcdc98de7a613
Allen Day <allenday@allenday.com>
2025-08-04 17:48:11 +0700

    chore(release): Update version to 0.1.2

--- src/a2a_registry/_version.py
+++ src/a2a_registry/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.2.dev14+ge31aa45e2.d20250804'
-__version_tuple__ = version_tuple = (0, 1, 2, 'dev14', 'ge31aa45e2.d20250804')
+__version__ = version = '0.1.2.dev15+gba641c877.d20250804'
+__version_tuple__ = version_tuple = (0, 1, 2, 'dev15', 'gba641c877.d20250804')
 
-__commit_id__ = commit_id = 'ge31aa45e2'
+__commit_id__ = commit_id = 'gba641c877'
