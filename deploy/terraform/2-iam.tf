52b391408a8197574ca65dbd206312a099a76617
Allen Day <allenday@allenday.com>
2025-08-05 11:31:59 +0700

    fix(terraform): Remove duplicate service account\n\n- Remove duplicate service account resource\n- Use existing service account from 0-service-account.tf

--- deploy/terraform/2-iam.tf
+++ deploy/terraform/2-iam.tf
@@ -1,28 +1,12 @@
-data "google_service_account" "tailscale_data" {
-  depends_on = [
-    time_sleep.wait_for_service_account
-  ]
-  account_id = google_service_account.tailscale.account_id
-}
-
 resource "time_sleep" "wait_for_service_account" {
   depends_on = [google_service_account.tailscale]
   create_duration = "30s"
 }
 
-resource "google_service_account" "tailscale" {
-  account_id   = "tailscale-sa"
-  display_name = "Tailscale Service Account"
-  lifecycle {
-    prevent_destroy = true
-    create_before_destroy = true
-  }
-}
-
 resource "google_project_iam_member" "tailscale_node" {
   project = var.project_id
   role    = "roles/container.nodeServiceAccount"
-  member  = "serviceAccount:${data.google_service_account.tailscale_data.email}"
+  member  = "serviceAccount:${data.google_service_account.tailscale.email}"
   depends_on = [
     time_sleep.wait_for_apis,
     time_sleep.wait_for_service_account
